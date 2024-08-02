a4f09e5fe60cd415d8416cb6bffde1674616509a
Nikita <kotovnikita00744@gmail.com>
2024-08-02 12:40:52 +0700

    Base configuration

---
+++ src/main/java/ru/axproject/ax_lip_gateway/AxLipGatewayApplication.java
@@ -0,0 +1,13 @@
+package ru.axproject.ax_lip_gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AxLipGatewayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AxLipGatewayApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ru/axproject/ax_lip_gateway/AxLipGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.axproject.ax_lip_gateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AxLipGatewayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
